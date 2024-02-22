ddaded36dbdd28d039fa1e3555c0352c5884f9d8
Peter Bridson <pbridson@memphis.edu>
2024-02-22 17:43:26 -0600

    Added statements to limit number of guesses

--- guess_number.py
+++ guess_number.py
@@ -1,10 +1,18 @@
 number = 10
 
 print("I'm thinking of a number...")
+count = 5
+print(f"You can have {count} guesses.")
 guess = int(input("What number am I thinking of? "))
+
 while guess != number:
   try:
-    guess = int(input("Sorry! Try again or enter q to quit: "))
+    count -= 1
+    print (f"Sorry, that's not it! You have {count} guesses left.")
+    if count == 0:
+      print(f"The number was {number}.")
+      break
+    guess = int(input("Try again or enter q to quit: "))
   except:
     print(f"You give up! The number was {number}.")
     break
