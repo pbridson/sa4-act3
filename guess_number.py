1526582f1858364aeb26572ff888baa0a736e743
Peter Bridson <pbridson@memphis.edu>
2024-02-22 16:39:52 -0600

    Corrected inpu to integer-type

--- guess_number.py
+++ guess_number.py
@@ -1,10 +1,11 @@
-number = "10"
+number = 10
 
 print("I'm thinking of a number...")
-guess = input("What number am I thinking of? ")
+guess = int(input("What number am I thinking of? "))
 while guess != number:
-  guess = input("Sorry! Try again or enter q to quit: ")
-  if guess == "q":
+  try:
+    guess = int(input("Sorry! Try again or enter q to quit: "))
+  except:
     print(f"You give up! The number was {number}.")
     break
 else:                 
