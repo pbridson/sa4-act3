879048e9e645aa4e09c31445b25dc1065468bbc2
Peter Bridson <pbridson@memphis.edu>
2024-02-22 16:28:29 -0600

    Reverted to integer type and added hints

--- guess_number.py
+++ guess_number.py
@@ -1,10 +1,12 @@
-number = "10"
+number = 10
 
 print("I'm thinking of a number...")
-guess = input("What number am I thinking of? ")
+guess = int(input("What number am I thinking of? "))
 while guess != number:
-  guess = input("Sorry! Try again or enter q to quit: ")
-  if guess == "q":
+  try:
+    hint = ("low" if guess < number else "high")
+    guess = int(input(f"Sorry, your guess is too {hint}! Try again or enter q to quit: "))
+  except:
     print(f"You give up! The number was {number}.")
     break
 else:                 
